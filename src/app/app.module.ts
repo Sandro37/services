8b141452145439036b1a309a9306f97fc52fe322
Sandro37 <alessandro37@outlook.com.br>
2021-11-30 20:36:06 -0300

    Injetando um serviço em outro serviço

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { CursosComponent } from './cursos/cursos.component';
 import { CursosService } from './cursos/cursos.service';
 import { CriarCursoComponent } from './criar-curso/criar-curso.component';
+import { LogService } from './shared/log.service';
 
 @NgModule({
   declarations: [
--- src/app/cursos/cursos.service.ts
+++ src/app/cursos/cursos.service.ts
@@ -1,13 +1,14 @@
 import { Injectable, EventEmitter } from "@angular/core";
-
+import { LogService } from "../shared/log.service";
 @Injectable()
 export class CursosService{
     emitirCursoCriado = new EventEmitter<string>();
 
      cursos:string[] = ["Angular", "Java", "C#", "JS", "Cobol"];
-    constructor(){}
+    constructor(private logService: LogService ){}
     
     getCursos(){
+        this.logService.consoleLog(`Obtendo lista de cursos ${this.cursos}`);
         return this.cursos;
     }
 
